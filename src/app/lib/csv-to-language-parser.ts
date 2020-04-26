e47b7f09946d3c2c552126df97563e4ca055090c
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2020-04-26 08:47:44 -0300

    Fixed exception when an empty csv is provided

--- src/app/lib/csv-to-language-parser.ts
+++ src/app/lib/csv-to-language-parser.ts
@@ -15,15 +15,18 @@ export class CsvToLanguageParser {
     this.variables = translator.typeMap();
 
     const map = new Map<string, string>();
-    const data = csv
-      .replace('(', '')
-      .replace(')', '')
-      .replace(new RegExp('[0-9]', 'g'), '')
-      .replace(new RegExp('\r?\n', 'g'), ',')
-      .split(',');
 
-    for (let i = 0; i < data.length; i += 2) {
-      map.set(data[i], this.translateType(data[i + 1].toUpperCase()));
+    if (csv) {
+      const data = csv
+        .replace('(', '')
+        .replace(')', '')
+        .replace(new RegExp('[0-9]', 'g'), '')
+        .replace(new RegExp('\r?\n', 'g'), ',')
+        .split(',');
+
+      for (let i = 0; i < data.length; i += 2) {
+        map.set(data[i], this.translateType(data[i + 1].toUpperCase()));
+      }
     }
 
     this.variables = map;
